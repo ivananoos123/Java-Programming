3e5ad2d45889958b930cb1c36e976c1009540631
Ivan <55166879+ivananoos123@users.noreply.github.com>
2020-12-28 12:16:34 +0800

    simple java exercises

---
+++ Easy Java Problems/Conditional/Sum.java
@@ -0,0 +1,13 @@
+public class Sum{
+	public static void sum(int num1,int num2){
+			int sum1=0;
+	      	sum1 = num1 + num2;
+       		System.out.println(sum1);
+	}
+
+	public static void main (String[] args){
+	System.out.println("sum(10,20)");
+	sum(10,20);
+	}
+}
+
---
+++ Easy Java Problems/Hello World/HelloWorld.java
@@ -0,0 +1,14 @@
+
+import java.util.*;
+
+/**
+ * HelloWorld
+ */
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+		Scanner sc = new Scanner(System.in);
+		byte units = sc.nextByte();
+	}
+}
